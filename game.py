d04d80a18e30da3554ebb3a428735b8c29826082
mor bargig <morbargig@gmail.com>
2020-06-01 16:06:00 +0300

    automatic commit

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ import time
 # import sys
 # print(sys.executable)
 # exit()
-width, heigth = 500, 480
+width, heigth = 1000, 960
 
 
 class BasicSprite(object):
@@ -42,13 +42,14 @@ for i in range(0, random.randrange(300)):
 
 eatSound = pygame.mixer.Sound('eatSound.wav')
 # pygame.mixer.music.sound.load('eatSound.wav')
-music = pygame.mixer.music.load('music.mp3')
-pygame.mixer.music.play(-1)
+# music = pygame.mixer.music.load('music.mp3')
+# pygame.mixer.music.play(-1)
 run = True
 loopNmber = 0
 joump = False
 moves = []
 moves2 = []
+computer= True
 showText = False
 losee = False
 while run:
@@ -57,6 +58,13 @@ while run:
     lasty = player.y
     lastx2 = player2.x
     lasty2 = player2.y
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    keys = pygame.key.get_pressed()
+    if losee and keys[pygame.K_p]:
+        # run = True
+        losee =False
     if not losee:
         if loopNmber % 5 == 0:
             loopNmber = 0
@@ -67,7 +75,6 @@ while run:
             if event.type == pygame.QUIT:
                 run = False
         # Handling events
-        keys = pygame.key.get_pressed()
         # if joump :
         #     player.y += 10
         #     joump = False
@@ -91,31 +98,73 @@ while run:
                 player.x -= 5
             else:
                 player.x = width - 5
-        if keys[pygame.K_w]:
-            if player2.y > 0:
-                player2.y -= 5
-            else:
-                player2.y = heigth - 5
-        if keys[pygame.K_s]:
-            if player2.y + player2.heigth < heigth:
-                player2.y += 5
-            else:
-                player2.y = 5
-        if keys[pygame.K_d]:
-            if player2.x + player2.width < width:
-                player2.x += 5
-            else:
-                player2.x = 5
-        if keys[pygame.K_a]:
-            if player2.x > 0:
-                player2.x -= 5
-            else:
-                player2.x = width - 5
+        # jump
         # if keys[pygame.K_SPACE] and player.y > 10:
         #     player.y -= 10
         #     joump=True
+        if not computer:
+            # player 2
+            if keys[pygame.K_w]:
+                if player2.y > 0:
+                    player2.y -= 5
+                else:
+                    player2.y = heigth - 5
+            if keys[pygame.K_s]:
+                if player2.y + player2.heigth < heigth:
+                    player2.y += 5
+                else:
+                    player2.y = 5
+            if keys[pygame.K_d]:
+                if player2.x + player2.width < width:
+                    player2.x += 5
+                else:
+                    player2.x = 5
+            if keys[pygame.K_a]:
+                if player2.x > 0:
+                    player2.x -= 5
+                else:
+                    player2.x = width - 5
+        enemieIndex = None
+        enemie = None
+        optionalEnemies = enemies[:]
+        
+        # bedEnemies =[]
+        # while(True):
+        actlist = [0,0]
+        def act():
+            minDistance = ((heigth**2) + (width**2) )**0.5
+            global actlist, enemie
+            actlist= [0,0]
+            for i in optionalEnemies:
+                newDis =  ( (abs(player2.x - i.x)**2) + (abs(player2.y - i.y)**2))**0.5
+                if newDis < minDistance:
+                    minDistance = newDis
+                    enemie = i
+            yDic = player2.y - enemie.y
+            xDic = player2.x - enemie.x
+            if abs(yDic) > abs(xDic):
+                if yDic > 0:
+                    actlist[0] -= 5
+                else :  actlist[0] += 5
+            elif xDic > 0 :
+                actlist[1]-=5
+            else : actlist[1]+=5
+            player2.y += actlist[0]
+            player2.x +=actlist[1]
+        def unAct ():
+            optionalEnemies.remove(enemie)
+            player2.y -= actlist[0]
+            player2.x -=actlist[1]
+        if computer:
+            act()
+        # print(enemieIndex)
+
 
         for i in enemies:
+            # newDis = ((abs(player2.x - i.x) + abs(player2.y - i.y))**2)**0.5
+            # if newDis < minDistance:
+            #     minDistance = newDis 
+
             if player.x <= i.x + i.width and player.x + player.width >= i.x:
                 if player.y <= i.y + i.heigth and player.y + player.heigth >= i.y:
                     eatSound.play()
@@ -134,39 +183,48 @@ while run:
                 break
             if (i.x, i.y) == (player.x, player.y):
                 font = pygame.font.Font('freesansbold.ttf', 32)
-                text = font.render('player 1 win !', True, (0, 255, 0), (0, 0, 255))
-                textRect = text.get_rect()
-                textRect.center = (width // 2, heigth // 2)
-                losee = True
-                showText = True
-                break
-            if (abs( i.x -player2.x) < player2.width ) and ( abs(i.y -player2.y)  < player2.heigth):
-                font = pygame.font.Font('freesansbold.ttf', 32)
                 text = font.render('player 2 win !', True, (0, 255, 0), (0, 0, 255))
                 textRect = text.get_rect()
                 textRect.center = (width // 2, heigth // 2)
                 losee = True
                 showText = True
                 break
+            if computer :
+                while(abs( i.x -player2.x) < player2.width ) and ( abs(i.y -player2.y)  < player2.heigth):
+                    unAct()
+                    act()
+            else:
+                if (abs( i.x -player2.x) < player2.width ) and ( abs(i.y -player2.y)  < player2.heigth):
+                    font = pygame.font.Font('freesansbold.ttf', 32)
+                    text = font.render('player 1 win !', True, (0, 255, 0), (0, 0, 255))
+                    textRect = text.get_rect()
+                    textRect.center = (width // 2, heigth // 2)
+                    losee = True
+                    showText = True
+                    break
             newx, newy = i.x, i.y
             i.x, i.y = lastx, lasty
             lastx, lasty = newx, newy
 
-
         for i in moves2:
             if (lastx2, lasty2) == (i.x, i.y) or (player2.x, player2.y) == (lastx2, lasty2):
                 break
-            if (i.x, i.y) == (player2.x, player2.y):
-                font = pygame.font.Font('freesansbold.ttf', 32)
-                text = font.render('Game Over', True, (0, 255, 0), (0, 0, 255))
-                textRect = text.get_rect()
-                textRect.center = (width // 2, heigth // 2)
-                losee = True
-                showText = True
-                break
+            if computer:
+                while (i.x, i.y) == (player2.x, player2.y):
+                    unAct()
+                    act()
+            else : 
+                if (i.x, i.y) == (player2.x, player2.y):
+                    font = pygame.font.Font('freesansbold.ttf', 32)
+                    text = font.render('player 1 win !', True, (0, 255, 0), (0, 0, 255))
+                    textRect = text.get_rect()
+                    textRect.center = (width // 2, heigth // 2)
+                    losee = True
+                    showText = True
+                    break
             if (abs(i.x -player.x) <player.width ) and (abs(i.y -player.y) <player.heigth):
                 font = pygame.font.Font('freesansbold.ttf', 32)
-                text = font.render('player 1 win !', True, (0, 255, 0), (0, 0, 255))
+                text = font.render('player 2 win !', True, (0, 255, 0), (0, 0, 255))
                 textRect = text.get_rect()
                 textRect.center = (width // 2, heigth // 2)
                 losee = True
@@ -176,16 +234,16 @@ while run:
             i.x, i.y = lastx2, lasty2
             lastx2, lasty2 = newx, newy
 
-    # Redraw screen
-    screen.fill((255, 255, 255))
-    for i in enemies:
-        i.draw(screen)
-    for i in moves:
-        i.draw(screen)
-    for i in moves2:
-        i.draw(screen)
-    player.draw(screen)
-    player2.draw(screen)
+        # Redraw screen
+        screen.fill((255, 255, 255))
+        for i in enemies:
+            i.draw(screen)
+        for i in moves:
+            i.draw(screen)
+        for i in moves2:
+            i.draw(screen)
+        player.draw(screen)
+        player2.draw(screen)
     clock.tick(30)
     if showText:
         print("game over")
